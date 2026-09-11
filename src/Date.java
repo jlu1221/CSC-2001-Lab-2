f6da73d10cfd7fb3edf51883c50a8c69f4fd1566
Jordan <jlu110@calpoly.edu>
2026-09-10 17:27:26 -0700

    added the constuctor and one method

--- src/Date.java
+++ src/Date.java
@@ -1,10 +1,24 @@
 public record Date(int year, int month, int day){
+    // Constructor
     public Date{
         if (month < 1 || month > 12){
             throw new IllegalArgumentException("Invalid month");
         }
-        if (day < 1){
+        if (day < 1 || day > daysInMonth(month)){
             throw new IllegalArgumentException(("Invalid day"));
         }
+
+    }
+
+    // daysInMonth method
+    static int daysInMonth(int month) {
+        return switch (month) {
+            case 1,3,5,7,8,10,12 -> 31;
+            case 4,6,9,11 -> 30;
+            case 2 -> 28;
+            default -> {
+                throw new IllegalArgumentException("Invalid month");
+            }
+        };
     }
 }
---
+++ src/TestRecords.java
@@ -0,0 +1,2 @@
+public class TestRecords {
+}
