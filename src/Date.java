f22ca612843d5bf5fc021b49ffa16fc1278af115
Jordan <jlu110@calpoly.edu>
2026-09-10 17:58:01 -0700

    added tomorrow method

--- src/Date.java
+++ src/Date.java
@@ -21,4 +21,26 @@ public record Date(int year, int month, int day){
             }
         };
     }
+
+    // main method
+    public static void main(String[] args) {
+        Date date1 = new Date(2026, 4, 30);
+        Date date2 = new Date(2026, 3,31);
+        Date date3 = new Date(2026, 2, 28);
+    }
+
+    // tomorrow method
+    static Date tomorrow(Date date) {
+        // Last day of the year
+        if (date.month() == 12 && date.day() == 31) {
+            return new Date(date.year() + 1, 1, 1);
+        }
+        // Last day of month
+        else if (date.day() == daysInMonth(date.month())) {
+            return new Date(date.year(), date.month() + 1, 1);
+        }
+        // Normal case
+        else
+            return new Date(date.year(), date.month(), date.day() + 1);
+    }
 }
