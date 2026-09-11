45749891c988950b5d8b75af36d6067a5f1a5c5a
Jordan <jlu110@calpoly.edu>
2026-09-10 23:44:37 -0700

    push

--- src/DateList.java
+++ src/DateList.java
@@ -1,1 +1,76 @@
 public record DateList(Date first, DateList rest) {
+    // listLen method
+    static int listLen(DateList list) {
+        if (list == null) {
+            return 0;
+        } else {
+            return 1 + listLen(list.rest());
+        }
+    }
+    // minDate method
+    static Date minDate(DateList list) {
+        if (list == null) {
+            return null;
+        }
+        if (list.rest() == null) {
+            return list.first();
+        }
+
+        Date restMin = minDate(list.rest());
+
+        if (Date.comesBefore(list.first(), restMin)) {
+            return list.first();
+        } else {
+            return restMin;
+        }
+    }
+
+    // dateCover method
+    static DateInterval dateCover(DateList list) {
+        if (list == null) {
+            return null;
+        }
+
+        Date latest = list.first();
+        DateList remaining = list.rest();
+
+        while (remaining != null) {
+            if (Date.comesBefore(latest, remaining.first())) {
+                latest = remaining.first();
+            }
+            remaining = remaining.rest();
+        }
+        return new DateInterval(minDate(list), Date.tomorrow((latest)));
+    }
+
+    // allTomorrows method
+    static DateList allTomorrows(DateList list) {
+        if (list == null) {
+            return null;
+        }
+        return new DateList(
+                Date.tomorrow(list.first()),
+                allTomorrows(list.rest())
+        );
+    }
+    // addToEnd method
+    static DateList addToEnd(DateList list, Date date) {
+        if (list == null) {
+            return new DateList(date, null);
+        }
+        return new DateList(
+                list.first(),
+                addToEnd(list.rest(), date)
+        );
+    }
+    // append method
+    static DateList append(DateList list1, DateList list2) {
+        if (list1 == null) {
+            return list2;
+        }
+        return new DateList(
+                list1.first(),
+                append(list1.rest(), list2)
+        );
+    }
+}
--- src/TestRecords.java
+++ src/TestRecords.java
@@ -1,2 +1,40 @@
 public class TestRecords {
+    public static void main(String[] args) {
+        // Date objects
+        Date date1 = new Date(2026, 4, 30);
+        Date date2 = new Date(2026, 3,31);
+        Date date3 = new Date(2026, 2, 28);
+
+        System.out.println(Date.daysInMonth(2));
+        System.out.println(Date.tomorrow(date2));
+        System.out.println(Date.dayOfYear(date1));
+        System.out.println(Date.comesBefore(date1, date2));
+
+        // DateInterval objects
+        DateInterval interval1 = new DateInterval(date3, date2);
+        DateInterval interval2 = new DateInterval(date2, date1);
+        DateInterval interval3 = new DateInterval(date3, date1);
+
+        System.out.println(DateInterval.dateIntervalDays(interval1));
+        System.out.println(DateInterval.dateOverlap(interval1, interval2));
+        System.out.println(DateInterval.dateIntervalIntersect(interval1, interval3));
+        System.out.println(DateInterval.maybeDateIntervalIntersect(interval1, interval2));
+
+        // DateList object
+        DateList list3 = new DateList(date3, null);
+        DateList list2 = new DateList(date1, list3);
+        DateList list1 = new DateList(date2, list2);
+
+        System.out.println(DateList.listLen(list1));
+        System.out.println(DateList.minDate(list1));
+        System.out.println(DateList.dateCover(list1));
+        System.out.println(DateList.allTomorrows(list1));
+        System.out.println(DateList.addToEnd(list1, new Date(2027, 1, 1)));
+
+        // append list
+        DateList list4 = new DateList(new Date(2027, 2, 1), null);
+
+        System.out.println(DateList.append(list1, list4));
+
+    }
 }
